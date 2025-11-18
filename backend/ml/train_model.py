52961c66496c8a1fc69c1898d0ea59de94afa6cf
kirillsst <jewaor@gmail.com>
2025-11-18 12:10:10 +0100

    feat: wrapped train_model.py in a function for tests

--- backend/ml/train_model.py
+++ backend/ml/train_model.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import mlflow
 import joblib
-import os 
+import os
 
 MODEL_DIR = os.path.join(os.path.dirname(__file__), "model")
 os.makedirs(MODEL_DIR, exist_ok=True)
@@ -14,46 +14,60 @@ mlflow.set_tracking_uri("sqlite:///mlflow.db")
 mlflow.set_experiment("my-first-experiment")
 mlflow.sklearn.autolog()
 
-# Load Iris dataset
-X, y = datasets.load_iris(return_X_y=True)
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-)
 
-params = {
-    "solver": "lbfgs",
-    "max_iter": 1000,
-    "multi_class": "auto",
-    "random_state": 8888,
-}
+def train_and_save_model():
+    # Load Iris dataset
+    X, y = datasets.load_iris(return_X_y=True)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
 
-with mlflow.start_run() as run:
-    lr = LogisticRegression(**params)
-    lr.fit(X_train, y_train)
+    params = {
+        "solver": "lbfgs",
+        "max_iter": 1000,
+        "multi_class": "auto",
+        "random_state": 8888,
+    }
 
-    # Construct the correct model URI for autologged model
-    model_uri = f"runs:/{run.info.run_id}/model"
+    with mlflow.start_run() as run:
+        lr = LogisticRegression(**params)
+        lr.fit(X_train, y_train)
 
-# Load the model
-loaded_model = mlflow.sklearn.load_model(model_uri)
+        # MLflow autologged model URI
+        model_uri = f"runs:/{run.info.run_id}/model"
 
-# Predict
-predictions = loaded_model.predict(X_test)
+    # Load model to check autologged save
+    loaded_model = mlflow.sklearn.load_model(model_uri)
 
-# Prepare results
-iris_feature_names = datasets.load_iris().feature_names
-result = pd.DataFrame(X_test, columns=iris_feature_names)
-result["actual_class"] = y_test
-result["predicted_class"] = predictions
+    # Predictions
+    predictions = loaded_model.predict(X_test)
 
-joblib.dump(lr, MODEL_PATH)
-print(f"Saved model.pkl in {MODEL_PATH}")
+    # Prepare results
+    iris_feature_names = datasets.load_iris().feature_names
+    result = pd.DataFrame(X_test, columns=iris_feature_names)
+    result["actual_class"] = y_test
+    result["predicted_class"] = predictions
+
+    # Save model for API
+    joblib.dump(lr, MODEL_PATH)
+    print(f"Saved model.pkl in {MODEL_PATH}")
+
+    return result, lr
 
 
-print(result[:4])
+if __name__ == "__main__":
+    df, model = train_and_save_model()
+    print(df[:4])
+
+## run python3 train_model.py
+# for train your model
 
 
-# run ui mlflow
+
+# run ui mlflow for create mlflow.db
 # mlflow ui \
 #   --backend-store-uri sqlite:///mlflow.db \
 #   --default-artifact-root ./mlruns
+
+
+
